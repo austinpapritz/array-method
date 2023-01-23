2f031674dc2f9574ca9c7cee9bb5d067daa595c7
Austin Papritz <austin@papritz.dev>
2023-01-23 10:04:08 -0800

    added methods from Diegos image

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-/* Imports */
-
-/* Get DOM Elements */
-
-/* State */
-
-/* Events */
-
-/* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
+[4, 5, 6, 7].at(1);
+[4, 5, 6, 7].push(8);
+[4, 5, 6, 7].pop();
+[4, 5, 6, 7].fill(1);
+[4, 5, 6, 7].join(' ');
+[4, 5, 6, 7].shift();
+[4, 5, 6, 7].reverse();
+[4, 5, 6, 7].unshift(3);
+[4, 5, 6, 7].includes(6);
+[4, 5, 6, 7].map((item) => 2 * item);
+[4, 5, 6, 7].filter((item) => item > 5);
+[4, 5, 6, 7].find((item) => item > 5);
+[4, 5, 6, 7].every((item) => item === 5);
+[4, 5, 6, 7].findIndex((item) => item === 5);
+[4, 5, 6, 7].reduce((prev, curr) => prev + curr, 0);
